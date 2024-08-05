d76ec5bf3ff95dd79460a33d83cfab78b5717d7f
winterbreaker22 <joejamesdev@hotmail.com>
2024-08-04 19:36:46 -0700

    Update model.py

--- model.py
+++ model.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 file_path = 'melb_data.csv'
 melb_data = pd.read_csv(file_path)
-print (melb_data.describe())
+melb_data = melb_data.dropna(axis=0)
+y = melb_data.Price
+print (y)
