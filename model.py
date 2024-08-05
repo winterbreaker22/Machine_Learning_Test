7c358cdb94498762b781dbf2ab59956e529705b8
winterbreaker22 <joejamesdev@hotmail.com>
2024-08-04 20:53:33 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,16 @@
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
 import pandas as pd
 
 file_path = 'melb_data.csv'
 melb_data = pd.read_csv(file_path)
 melb_data = melb_data.dropna(axis=0)
 y = melb_data.Price
-print (y)
+
+melb_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = melb_data[melb_features]
+
+melb_model = DecisionTreeRegressor(random_state=1)
+melb_model.fit(X, y)
+print (X.head())
+print (melb_model.predict(X.head()))
